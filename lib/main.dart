a14e73f67d91a0ffb3d408e58d81077d0be35a9a
Vedant Gadekar <vedantgadekar77@gmail.com>
2024-06-21 00:36:03 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:valyrian_ventures/pages/profile.dart';
 import 'package:valyrian_ventures/pages/signup.dart';
 
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home:Profile(),
+        home:Signup(),
 
     );
   }
