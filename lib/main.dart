0b16ac8c3ae28175097a761e6ee5b41b925e5e02
Vedant Gadekar <vedantgadekar77@gmail.com>
2024-06-21 00:39:50 +0530

    profile updated in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:valyrian_ventures/pages/profile.dart';
+import 'package:valyrian_ventures/pages/signup.dart';
 // import 'package:valyrian_ventures/pages/signup.dart';
 
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home:Profile(),
+        home:Signup(),
 
     );
   }
