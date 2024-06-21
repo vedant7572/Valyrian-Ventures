167ad5078ba6e8ef64958356779cbc9f093dad21
Vedant Gadekar <vedantgadekar77@gmail.com>
2024-06-21 20:16:22 +0530

    check

--- lib/service/database.dart
+++ lib/service/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//changes aataa
 class DatabaseMethods{
   Future addUserDetails(Map<String,dynamic>userInfoMap, String Id) async{
     return await FirebaseFirestore.instance
